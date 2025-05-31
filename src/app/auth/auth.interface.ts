41f6d08bef3813f2376a1409649e320a160f7cc2
HS-OleksandrSuddia <oleksandr.suddia@hitek-soft.com>
2025-05-31 12:49:39 +0300

    1:45:52 fixed routing: now first page of the site is login page

---
+++ src/app/auth/auth.interface.ts
@@ -0,0 +1,4 @@
+export interface TokenResponse {
+  acsessToken: string;
+  refreshToken: string;
+}
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -3,6 +3,7 @@ import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/
 import { CommonModule } from '@angular/common';
 import {AuthService} from "../../auth/auth.service";
 import {from, map, tap} from "rxjs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
@@ -15,34 +16,20 @@ import {from, map, tap} from "rxjs";
 })
 export class LoginPageComponent {
   authService = inject(AuthService)
+  router = inject(Router)
 
   form = new FormGroup({
     username: new FormControl<string | null>(null, Validators.required),
     password: new FormControl<string | null>(null, Validators.required)
   })
 
-  constructor() {
-    from([1, 2, 3, 4, 5, 6, 7, 8, 9])
-      .pipe(
-        map( val => val * 2),
-        tap( val => {
-          this.form.patchValue({username: val.toString()})
-        })
-
-      )
-
-
-
-
-      .subscribe(val => {
-      console.log(val)
-    })
-  }
 
   onSubmit() {
     if (this.form.valid) {
       //@ts-ignore
-      this.authService.login(this.form.value).subscribe(res => {
+      this.authService.login(this.form.value)
+        .subscribe(res => {
+          this.router.navigate([''])
         console.log(res)
       });
     }
