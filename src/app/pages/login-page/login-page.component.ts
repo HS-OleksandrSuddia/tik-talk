e5686f8bc9e4786d7e278223c2b681faa6ec7ca7
HS-OleksandrSuddia <oleksandr.suddia@hitek-soft.com>
2025-05-31 12:25:39 +0300

    31.05 commit

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -2,6 +2,7 @@ import {Component, inject} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import { CommonModule } from '@angular/common';
 import {AuthService} from "../../auth/auth.service";
+import {from, map, tap} from "rxjs";
 
 @Component({
   selector: 'app-login-page',
@@ -16,14 +17,34 @@ export class LoginPageComponent {
   authService = inject(AuthService)
 
   form = new FormGroup({
-    username: new FormControl(null, Validators.required),
-    password: new FormControl(null, Validators.required)
+    username: new FormControl<string | null>(null, Validators.required),
+    password: new FormControl<string | null>(null, Validators.required)
   })
+
+  constructor() {
+    from([1, 2, 3, 4, 5, 6, 7, 8, 9])
+      .pipe(
+        map( val => val * 2),
+        tap( val => {
+          this.form.patchValue({username: val.toString()})
+        })
+
+      )
+
+
+
+
+      .subscribe(val => {
+      console.log(val)
+    })
+  }
 
   onSubmit() {
     if (this.form.valid) {
       //@ts-ignore
-      this.authService.login(this.form.value).subscribe();
+      this.authService.login(this.form.value).subscribe(res => {
+        console.log(res)
+      });
     }
   }
 }
