75bafd6c9f0ad7e3c9fe2850c5185c4144c35bc0
HS-OleksandrSuddia <oleksandr.suddia@hitek-soft.com>
2025-07-04 13:13:07 +0300

    search and chat icons are not working

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,15 +1,35 @@
 import { Component } from '@angular/core';
 import {SvgIconComponent} from "../svg-icon/svg-icon.component";
+import {RouterLink} from "@angular/router";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [
-    SvgIconComponent
+    SvgIconComponent,
+    RouterLink,
+    NgForOf
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-
+  menuItems = [
+    {
+      label: 'Моя страница',
+      icon: 'home',
+      link: ''
+    },
+    {
+      label: 'Чаты',
+      icon: 'chats',
+      link: 'chats'
+    },
+    {
+      label: 'Поиск',
+      icon: 'search',
+      link: 'search'
+    },
+  ]
 }
