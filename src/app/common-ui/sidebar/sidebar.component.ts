78f8b5a0275660b25a32a075b64715a363d0781c
HS-OleksandrSuddia <oleksandr.suddia@hitek-soft.com>
2025-07-04 14:25:06 +0300

    arrow sidebar

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {SvgIconComponent} from "../svg-icon/svg-icon.component";
 import {RouterLink} from "@angular/router";
 import {NgForOf} from "@angular/common";
+import {SubscriberCardComponent} from "./subscriber-card/subscriber-card.component";
 
 @Component({
   selector: 'app-sidebar',
@@ -9,7 +10,8 @@ import {NgForOf} from "@angular/common";
   imports: [
     SvgIconComponent,
     RouterLink,
-    NgForOf
+    NgForOf,
+    SubscriberCardComponent
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
